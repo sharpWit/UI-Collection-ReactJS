5c866624f538be9041384f1876b6881cec26f77a
Saeed Khosravi <91289174+sharpWit@users.noreply.github.com>
2024-02-11 04:01:50 +0330

    fix addColor

--- src/components/header/Navigation.tsx
+++ src/components/header/Navigation.tsx
@@ -1,4 +1,8 @@
 const Navigation = () => {
-  return <nav className="container font-semibold">React's UI Collection</nav>;
+  return (
+    <nav className="container font-semibold">
+      <h1 className="font-bold text-xl">React&apos;s UI Collection</h1>
+    </nav>
+  );
 };
 export default Navigation;
